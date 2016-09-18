1f453b4f2ce5b95f715b3b178ce32c613804de6e
Tech-wizard <876424077@qq.com>
2016-09-18 21:57:26 +0800

    循环第二个for 跳出

--- main.ts
+++ main.ts
@@ -1,10 +1,14 @@
 function print(x:number):void {
 
     for(var j; j<x; j++){
-    for(var i; i<2*x-1; i++){
+       for(var k=0;k<x-j-1;k++){
+            console.log(" ")
+        }
+            for(var i; i<(2*x-1)+1; i++){
+       
         console.log("*");
     }
-     console.log("\t");
+     console.log("\n");
     }
 }
 

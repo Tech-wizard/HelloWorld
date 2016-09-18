cc3c1717d6d6b324d7c5406bf66d5ac89efdd174
Tech-wizard <876424077@qq.com>
2016-09-18 22:36:18 +0800

    Finish Homework 2

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 function print(x:number):void {
- var str : string  = "";
+ var str : string  = "";  
 
     for(var j=0; j<x; j++){
         str="";
@@ -9,7 +9,7 @@ function print(x:number):void {
        for(var i=0; i<2*j+1; i++){
             str+="*";
         }
-     console.log(str);
+     console.log(str);  //因为换行所以每行用string和在一起输出一次
     }
 }
 

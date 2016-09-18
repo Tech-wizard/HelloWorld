124cf770c57d9657c596e0a3d5c12e7669dc592a
Tech-wizard <876424077@qq.com>
2016-09-18 18:48:38 +0800

    no message

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 function print(x:number):void {
-    for(var j;j<x;j++){
-    for(var i;i<2x-1;i++){
+
+    for(var j; j<x; j++){
+    for(var i; i<2*x-1; i++){
         console.log("*");
     }
      console.log("\t");
